ea9e46b47176be203f02951da630f253638af7fa
daniel <daniel.huang@hover.to>
2019-11-22 14:25:08 -0800

    if null or empty string is passed, reset to main activity

--- android/src/main/java/com/reactlibrary/RNChangeIconModule.java
+++ android/src/main/java/com/reactlibrary/RNChangeIconModule.java
@@ -28,16 +28,35 @@ public class RNChangeIconModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void changeIcon(String enableIcon, Promise promise) {
         final Activity activity = getCurrentActivity();
+        String mainActivity = this.packageName + ".MainActivity";
+
+        if (this.componentClass == null) this.componentClass = activity.getComponentName().getClassName();
+
+        // reset to main activity if enableIcon is null or undefined
         if (activity == null || enableIcon == null || enableIcon.isEmpty()) {
-            promise.reject("Icon string is empty.");
+            promise.resolve(true);
+            if(this.componentClass != mainActivity) {
+                activity.getPackageManager().setComponentEnabledSetting(
+                        new ComponentName(this.packageName, mainActivity),
+                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP
+                );
+                activity.getPackageManager().setComponentEnabledSetting(
+                        new ComponentName(this.packageName, this.componentClass),
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP
+                );
+                this.componentClass = mainActivity;
+            }
             return;
         }
-        if (this.componentClass == null) this.componentClass = activity.getComponentName().getClassName();
+
         String activeClass = this.packageName + ".MainActivity" + enableIcon;
         if (this.componentClass.equals(activeClass)) {
             promise.reject("Icon already in use.");
             return;
         }
+
         promise.resolve(true);
         activity.getPackageManager().setComponentEnabledSetting(
                 new ComponentName(this.packageName, activeClass),
